a003dbf61ab8ebb9c9045b3fa83d9da6fcb72e14
jeffx-3 <geoffreygroovy@gmail.com>
2024-12-09 17:59:28 +0300

    configured the project for deployment

--- hustlehub/settings.py
+++ hustlehub/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i2fc^1z#zp1ab059+20@n8-rr97=w_!h8ti&vj_x%xhfic0*7^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hustlehub-0ie2.onrender.com']
 
 
 # Application definition
