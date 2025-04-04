609b3a043f8c95f2083d25096846bf81ad2c9ea9
geoffrey <geoffreygroovy@gmail.com>
2025-04-04 14:41:07 +0300

    prod

--- hustlehub/asgi.py
+++ hustlehub/asgi.py
@@ -14,7 +14,7 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hustlehub.settings')
 
 application = get_asgi_application()
-application = application
+app = application
 
 
 
